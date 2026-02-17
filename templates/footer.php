54be1ada2405769e1cf0db369c778b765f4fcc05
Hexxxxxxx <67011212055@msu.ac.th>
2026-02-18 01:58:57 +0700

    fix: undefined variable isLoggedIn in footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -32,12 +32,13 @@
             }
         }
     </script>
+    <?php $isLoggedInFooter = !empty(getCurrentUser()); ?>
     <nav class="md:hidden fixed bottom-0 w-full bg-white border-t-2 border-black p-2 flex justify-around rounded-t-xl z-50">
         <a href="/explore" class="flex flex-col items-center p-2 <?= ($activePage ?? '') === 'explore' ? 'text-black' : 'text-gray-400' ?>">
             <span class="material-symbols-outlined text-xl">search</span>
             <span class="text-[10px] font-bold">ค้นหา</span>
         </a>
-        <?php if ($isLoggedIn): ?>
+        <?php if ($isLoggedInFooter): ?>
         <a href="/my-events" class="flex flex-col items-center p-2 <?= ($activePage ?? '') === 'my-events' ? 'text-black' : 'text-gray-400' ?>">
             <span class="material-symbols-outlined text-xl">calendar_month</span>
             <span class="text-[10px] font-bold">กิจกรรม</span>
