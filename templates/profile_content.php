dc9e06f28861d926cb72f8467d9106c28555f98e
Hexxxxxxx <67011212055@msu.ac.th>
2026-02-18 04:03:19 +0700

    style: update sound toggle to match neo-brutalism theme

--- templates/profile_content.php
+++ templates/profile_content.php
@@ -56,7 +56,7 @@
                 <span class="text-sm text-gray-500">เสียงเอฟเฟกต์</span>
                 <label class="relative inline-flex items-center cursor-pointer">
                     <input type="checkbox" id="soundToggle" class="sr-only peer">
-                    <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
+                    <div class="w-12 h-7 bg-white border-2 border-black shadow-[2px_2px_0px_0px_black] peer-checked:bg-[#FFE600] peer-checked:shadow-[2px_2px_0px_0px_black] rounded-full peer after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-2 after:border-black after:rounded-full after:h-5 after:w-5 after:shadow-[1px_1px_0px_0px_black] after:transition-all peer-checked:after:translate-x-5 peer-checked:after:bg-white"></div>
                 </label>
             </div>
         </div>
